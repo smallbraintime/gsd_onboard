cf1246c8d7dfac78756bfd348dedc766727d7a6c
Patryk Strużek <patrykstruzek@proton.me>
2026-01-18 16:39:21 +0100

    chore: increase connectionTimeoutMs

--- src/MavSocket.h
+++ src/MavSocket.h
@@ -23,7 +23,7 @@ class MavSocket : public gsd::IMavSocket {
         uint16_t port = 14550;
         bool wifiLongRange = false;
         bool ssidHidden = false;
-        uint32_t connectionTimeoutMs = 5000;
+        uint32_t connectionTimeoutMs = 15000;
     };
 
     MavSocket(const MavSocket&) = delete;
